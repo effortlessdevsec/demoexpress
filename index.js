3e26f62b8eea86587e8f6dceecee4765265c7887
Effortless <30777722+effortlessdevsec@users.noreply.github.com>
2024-06-05 12:53:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
     res.json("hello world");
 });
 
-app.listen(8000, () => {
-    console.log("Server is running on port 8000");
+app.listen(8080, () => {
+    console.log("Server is running on port 8080");
 });
